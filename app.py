5cef8c5515308294b992924c24e9a196d85785d0
shekhar0603 <chadharyshekhar03@gmail.com>
2023-10-22 11:38:57 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template('index.html')
+# check by URL : {URL}:{PORT}{route}
+
+@app.route('/weatherapp', methods =['POST','GET'])
+def weatherapp():
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {
+        'q' :request.form.get('city'),
+        'units' :request.form.get('units'),
+        'appid' :request.form.get('apiid'),
+        }
+    
+    response = requests.get(url,params=params)
+    data = response.json()
+    return f"data: {data}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003)
